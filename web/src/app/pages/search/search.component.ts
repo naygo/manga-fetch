f0103d128d2f8a96b076da61834c5132478a89b4
Nayla Gomes <nayla.cgs@gmail.com>
2024-03-19 19:55:19 -0300

    feat: add first components

---
+++ web/src/app/pages/search/search.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './search.component.html',
+})
+export class SearchComponent {}
