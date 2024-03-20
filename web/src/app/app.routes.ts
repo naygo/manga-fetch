82cc064bfd490f5746d5a900ff25d33279286b2f
Nayla Gomes <nayla.cgs@gmail.com>
2024-03-19 23:58:04 -0300

    feat: show routes with ngFor and style active routes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './pages/search/search.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   {
     path: '',
+    title: 'search',
     component: SearchComponent,
+  },
+  {
+    path: 'about',
+    title: 'about',
+    component: AboutComponent,
   },
 ];
